505a5f27a194a7bc08064331e4de485a31effc1a
Eric Jones <joneserice@gmail.com>
2013-09-14 16:47:00 -0500

    additional smaller changes

--- config/environment.rb
+++ config/environment.rb
@@ -5,4 +5,4 @@ require File.expand_path('../application', __FILE__)
 InterstateImaging::Application.initialize!
 
 #To stop using Quirks document mode in IE and use standards by setting to Strict in application.html.haml - this hsould stop HTML5 from always being the default w/ rails 3
-#Haml::Template.options[:format] = :xhtml
+Haml::Template.options[:format] = :xhtml
